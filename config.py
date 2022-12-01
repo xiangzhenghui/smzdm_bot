ad4c6570c56a855e4b2249705dda73c1372ff121
tiaowen <49056479+xiangzhenghui@users.noreply.github.com>
2022-12-01 16:57:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=iSq22n5N2WxyOC72KC6sgY97; device_id=23453680316627166366622747c299d65f4cb82cd75450839864d747a; __jsluid_s=57df64f79ec9ba90d6adcd5248771fe2; homepage_sug=a; r_sort_type=score; smzdm_user_source=1401B5C04B9C31CA68D93755B02F4CCD; sess=BA-hT9D1KRzcsbQRa8s1pXiBqCU6fLi1jv7bIYR2BJYsheylz3TQqtSgGyjxZEczYL4hWr76eXcBNPg5T5EHQjNbmEayqJYwLFJP7jZqSeuu2cWK65iz6FQRrc%3D; user=user%3A7731339419%7C7731339419; smzdm_id=7731339419; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1668396978,1669884348; smzdm_user_view=247E534766C79BBE1F225FAA84D5D7F8; _zdmA.vid=*; footer_floating_layer=0; ad_date=1; bannerCounter=%5B%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%5D; ad_json_feed=%7B%7D; _zdmA.time=1669884371854.7513.https%3A%2F%2Fwww.smzdm.com%2F; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%227731339419%22%2C%22first_id%22%3A%2218321a268d5450-0cd49fda845e18-3e604809-2073600-18321a268d66ea%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E8%87%AA%E7%84%B6%E6%90%9C%E7%B4%A2%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fwww.baidu.com%2Flink%22%2C%22%24latest_landing_page%22%3A%22https%3A%2F%2Fwww.smzdm.com%2F%22%7D%2C%22%24device_id%22%3A%2218321a268d5450-0cd49fda845e18-3e604809-2073600-18321a268d66ea%22%7D; _zdmA.uid=ZDMA.mokQ5Gzeo.1669884689.2419200; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1669884689; amvid=090a1fab9ac17d03d4bd46fbf2b82af4'
 
 
 """
